68c410bca346e82310a9811b7685f725a4c046f5
Sunnie <zixianma@ZixiandeMacBook-Pro.local>
2020-04-04 23:38:46 +0800

    odified html and js for displaying the new cases map

--- map.js
+++ map.js
@@ -1,4 +1,3 @@
-// function createMap(){
 var xmlhttp = new XMLHttpRequest();
 xmlhttp.onreadystatechange = function() {
   if (this.readyState == 4 && this.status == 200) {
@@ -14,30 +13,6 @@ xmlhttp.onreadystatechange = function() {
           highlightBorderWidth: 3
         },
         // tutorial for filling a map with a graditent of colors https://www.youtube.com/watch?v=_sqqxcRf9hw
-      //   fills: {
-      //   '0.1': '#CCFFCC',
-      //   '0.2': '#CCFFCC',
-      //   '0.3': '#99FF99',
-      //   '0.4': '#99FF99',
-      //   '0.5': '#66FF66',
-      //   '0.6': '#66FF66',
-      //   '0.7': '#33FF33',
-      //   '0.8': '#33FF33',
-      //   '0.9': '#00FF00',
-      //   '1.0': '#00FF00',
-      //   '0.0': '#FFFFCC',
-      //   '-0.1': '#FFCCCC',
-      //   '-0.2': '#FFCCCC',
-      //   '-0.3': '#FF9999',
-      //   '-0.4': '#FF9999',
-      //   '-0.5': '#FF6666',
-      //   '-0.6': '#FF6666',
-      //   '-0.7': '#FF3333',
-      //   '-0.8': '#FF3333',
-      //   '-0.9': '#FF0000',
-      //   '-1.0': '#FF0000',
-      //   defaultFill: '#FFFFFF'
-      // },
       fills:{
         'strong positive':'#F2543D',
         'weak positive':'#F28268',
@@ -49,57 +24,194 @@ xmlhttp.onreadystatechange = function() {
       data:dataObj
       });
       sentiment.labels();
+    }
+  }
+  xmlhttp.open("GET", "https://raw.githubusercontent.com/covid19-sentiment/covid19-sentiment.github.io/master/state_sentiment_example3.json", true);
+  xmlhttp.send();
 
 
-      var sentiment_1 = new Datamap({
+  // var xmlhttp_1 = new XMLHttpRequest();
+  // xmlhttp_1.onreadystatechange = function() {
+  //   if (this.readyState == 4 && this.status == 200) {
+  //     var dataObj = JSON.parse(this.responseText);
+  //     var sentiment_1 = new Datamap({
+  //       scope: 'usa',
+  //       element: document.getElementById('map_1'),
+  //       geographyConfig: {
+  //         highlightBorderColor: '#bada55',
+  //        popupTemplate: function(geography, data) {
+  //           return '<div class="hoverinfo">' + geography.properties.name + ' Sentiment:' +  data.sentimentVal + ' '
+  //         },
+  //         highlightBorderWidth: 3
+  //       },
+  //     fills:{
+  //       'strong positive':'#F280AA',
+  //       'weak positive':'#F2C4D0',
+  //       "neutral":'#F2E0C9',
+  //       "weak negative":'#8DF2F2',
+  //       "strong negative":'#77C7D9',
+  //       defaultFill: '#FFFFFF'
+  //     },
+  //     data:dataObj
+  //     });
+  //     sentiment_1.labels();
+  //     }
+  //   }
+  //   xmlhttp_1.open("GET", "https://raw.githubusercontent.com/covid19-sentiment/covid19-sentiment.github.io/master/state_sentiment_example3.json", true);
+  //   xmlhttp_1.send();
+
+    var xmlhttp_1 = new XMLHttpRequest();
+    xmlhttp_1.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        var dataObj = JSON.parse(this.responseText);
+        var new_cases = new Datamap({
+          scope: 'usa',
+          element: document.getElementById('map_1'),
+          geographyConfig: {
+            highlightBorderColor: '#bada55',
+           popupTemplate: function(geography, data) {
+              return '<div class="hoverinfo">' + geography.properties.name + 'New Cases:' +  data.newCases + ' '
+            },
+            highlightBorderWidth: 3
+          },
+        fills:{ //#ed2e38
+          'level_0':'#ffe0db',
+          'level_1':'#ffc1b8',
+          "level_2":'#ffa196',
+          "level_3":'#ff8175',
+          // "level_3":'#f75d56',
+          "level_4":'#ed2e38',
+          defaultFill: '#FFFFFF'
+        },
+        data:dataObj
+        });
+        new_cases.labels();
+        }
+      }
+      xmlhttp_1.open("GET", "https://raw.githubusercontent.com/covid19-sentiment/covid19-sentiment.github.io/master/python/data_processing/result/state_new_cases_example_wk1.json", true);
+      xmlhttp_1.send();
+  
+var xmlhttp_2 = new XMLHttpRequest();
+xmlhttp_2.onreadystatechange = function() {
+  if (this.readyState == 4 && this.status == 200) {
+    var dataObj = JSON.parse(this.responseText);
+    var new_cases = new Datamap({
+      scope: 'usa',
+      element: document.getElementById('map_2'),
+      geographyConfig: {
+        highlightBorderColor: '#bada55',
+        popupTemplate: function(geography, data) {
+          return '<div class="hoverinfo">' + geography.properties.name + 'New Cases:' +  data.newCases + ' '
+        },
+        highlightBorderWidth: 3
+      },
+    fills:{ //#ed2e38
+      'level_0':'#ffe0db',
+      'level_1':'#ffc1b8',
+      "level_2":'#ffa196',
+      "level_3":'#ff8175',
+      // "level_3":'#f75d56',
+      "level_4":'#ed2e38',
+      defaultFill: '#FFFFFF'
+    },
+    data:dataObj
+    });
+    new_cases.labels();
+    }
+  }
+  xmlhttp_2.open("GET", "https://raw.githubusercontent.com/covid19-sentiment/covid19-sentiment.github.io/master/python/data_processing/result/state_new_cases_example_wk2.json", true);
+  xmlhttp_2.send();
+
+  var xmlhttp_3 = new XMLHttpRequest();
+  xmlhttp_3.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      var dataObj = JSON.parse(this.responseText);
+      var new_cases = new Datamap({
         scope: 'usa',
-        element: document.getElementById('map_1'),
+        element: document.getElementById('map_3'),
         geographyConfig: {
           highlightBorderColor: '#bada55',
          popupTemplate: function(geography, data) {
-            return '<div class="hoverinfo">' + geography.properties.name + ' Sentiment:' +  data.sentimentVal + ' '
+            return '<div class="hoverinfo">' + geography.properties.name + 'New Cases:' +  data.newCases + ' '
           },
           highlightBorderWidth: 3
         },
-        // tutorial for filling a map with a graditent of colors https://www.youtube.com/watch?v=_sqqxcRf9hw
-      //   fills: {
-      //   '0.1': '#CCFFCC',
-      //   '0.2': '#CCFFCC',
-      //   '0.3': '#99FF99',
-      //   '0.4': '#99FF99',
-      //   '0.5': '#66FF66',
-      //   '0.6': '#66FF66',
-      //   '0.7': '#33FF33',
-      //   '0.8': '#33FF33',
-      //   '0.9': '#00FF00',
-      //   '1.0': '#00FF00',
-      //   '0.0': '#FFFFCC',
-      //   '-0.1': '#FFCCCC',
-      //   '-0.2': '#FFCCCC',
-      //   '-0.3': '#FF9999',
-      //   '-0.4': '#FF9999',
-      //   '-0.5': '#FF6666',
-      //   '-0.6': '#FF6666',
-      //   '-0.7': '#FF3333',
-      //   '-0.8': '#FF3333',
-      //   '-0.9': '#FF0000',
-      //   '-1.0': '#FF0000',
-      //   defaultFill: '#FFFFFF'
-      // },
-      fills:{
-        'strong positive':'#F280AA',
-        'weak positive':'#F2C4D0',
-        "neutral":'#F2E0C9',
-        "weak negative":'#8DF2F2',
-        "strong negative":'#77C7D9',
+      fills:{ //#ed2e38
+        'level_0':'#ffe0db',
+        'level_1':'#ffc1b8',
+        "level_2":'#ffa196',
+        "level_3":'#ff8175',
+        // "level_3":'#f75d56',
+        "level_4":'#ed2e38',
         defaultFill: '#FFFFFF'
       },
       data:dataObj
       });
-      sentiment_1.labels();
-  }
-};
+      new_cases.labels();
+      }
+    }
+    xmlhttp_3.open("GET", "https://raw.githubusercontent.com/covid19-sentiment/covid19-sentiment.github.io/master/python/data_processing/result/state_new_cases_example_wk3.json", true);
+    xmlhttp_3.send();
 
-xmlhttp.open("GET", "https://raw.githubusercontent.com/covid19-sentiment/covid19-sentiment.github.io/master/state_sentiment_example3.json", true);
-xmlhttp.send();
-// }
+    var xmlhttp_4 = new XMLHttpRequest();
+    xmlhttp_4.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        var dataObj = JSON.parse(this.responseText);
+        var new_cases = new Datamap({
+          scope: 'usa',
+          element: document.getElementById('map_4'),
+          geographyConfig: {
+            highlightBorderColor: '#bada55',
+           popupTemplate: function(geography, data) {
+              return '<div class="hoverinfo">' + geography.properties.name + 'New Cases:' +  data.newCases + ' '
+            },
+            highlightBorderWidth: 3
+          },
+        fills:{ //#ed2e38
+          'level_0':'#ffe0db',
+          'level_1':'#ffc1b8',
+          "level_2":'#ffa196',
+          "level_3":'#ff8175',
+          // "level_3":'#f75d56',
+          "level_4":'#ed2e38',
+          defaultFill: '#FFFFFF'
+        },
+        data:dataObj
+        });
+        new_cases.labels();
+        }
+      }
+      xmlhttp_4.open("GET", "https://raw.githubusercontent.com/covid19-sentiment/covid19-sentiment.github.io/master/python/data_processing/result/state_new_cases_example_wk4.json", true);
+      xmlhttp_4.send();
+
+
+      var xmlhttp_5 = new XMLHttpRequest();
+      xmlhttp_5.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+          var dataObj = JSON.parse(this.responseText);
+          var new_cases = new Datamap({
+            scope: 'usa',
+            element: document.getElementById('map_5'),
+            geographyConfig: {
+              highlightBorderColor: '#bada55',
+             popupTemplate: function(geography, data) {
+                return '<div class="hoverinfo">' + geography.properties.name + 'New Cases:' +  data.newCases + ' '
+              },
+              highlightBorderWidth: 3
+            },
+          fills:{ //#ed2e38
+            'level_0':'#ffe0db',
+            'level_1':'#ffc1b8',
+            "level_2":'#ffa196',
+            "level_3":'#ff8175',
+            // "level_3":'#f75d56',
+            "level_4":'#ed2e38',
+            defaultFill: '#FFFFFF'
+          },
+          data:dataObj
+          });
+          new_cases.labels();
+          }
+        }
+        xmlhttp_5.open("GET", "https://raw.githubusercontent.com/covid19-sentiment/covid19-sentiment.github.io/master/python/data_processing/result/state_new_cases_example_wk5.json", true);
+        xmlhttp_5.send();
